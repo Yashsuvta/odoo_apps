ef4c9f3fc3112a4e6f6af980ed77fd5efc2e467f
yash-odoo <yash.tis420@gmail.com>
2025-06-06 12:49:54 +0530

    update price of module

--- vehicle_rental_system/__manifest__.py
+++ vehicle_rental_system/__manifest__.py
@@ -37,7 +37,7 @@
     'application': False,
     "license": "OPL-1",
     "auto_install": False,
-    "price": 1,
+    "price": 0.5,
     "currency": "USD",
     'images': ['static/description/vehicle_rental_banner.png'],
 }
