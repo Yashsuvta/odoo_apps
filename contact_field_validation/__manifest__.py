a6a2c8973025da75491e13465cbd9eababc155ef
yashsuvta <yash.suvta@ethicsgroup.in>
2026-05-20 12:13:22 +0530

    add contact validation module

--- contact_field_validation/__manifest__.py
+++ contact_field_validation/__manifest__.py
@@ -4,11 +4,13 @@
     "name": "Contact Field Validation",
     "version": "17.0.1.0.0",
     "summary": "Frontend and backend validation for phone and email fields",
-    "description": """Frontend and backend validation for phone and email fields""",
+    "description": """
+Frontend and backend validation for phone and email fields.
+    """,
     "author": "CodeFusion OdooWorks",
     "maintainer": "CodeFusion OdooWorks",
     "support": "yashsuvta1236@gmail.com",
-    "website": "https://codefusion-odooworks.odoo.com/",
+    "website": "https://codefusion-odooworks.odoo.com",
     "category": "Tools",
     "license": "LGPL-3",
     "depends": [
@@ -28,6 +30,4 @@
     "installable": True,
     "application": False,
     "auto_install": False,
-    "price": 0,
-    "currency": "USD",
 }
